3da4f9c99f69f46f3065cd51f790b417b8262806
lars34932 <34932@ma-web.nl>
2023-09-07 14:28:08 +0200

    footer

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="nl">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <title>Document</title>
+    <title>tweakers</title>
 </head>
 <body>
     <header class="header">
