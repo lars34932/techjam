d6656118d4e610ff61bf63e01231cc77599f9cab
lars34932 <34932@ma-web.nl>
2023-09-07 13:11:39 +0200

    header klaar

---
+++ connect.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+function db_connect(): object
+{
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "techjam";
+
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $e) {
+        die ("Fout bij het maken van de connection: " . $e->getMessage());
+    }
+    return $conn;
+}
---
+++ index.php
@@ -0,0 +1,42 @@
+<?php include "connect.php"?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <header class="header">
+        <section class="header__section1">
+            <img class="header__logo" src="img/logo.webp" alt="logo">
+            <button class="header__button">Nieuws</button>
+            <button class="header__button">Reviews</button>
+            <button class="header__button">Prijsen</button>
+            <button class="header__button">Vraag en aanbod</button>
+            <button class="header__button">Forum</button>
+        </section>
+        <section class="header__section2">
+            <input class="header__search" type="search" placeholder="Zoek in de site">
+        </section>
+    </header>
+    <main>
+        <article class="news__article">
+            <img src="" alt="" class="article__img">
+            <h2 class="article__title"></h2>
+            <p class="article__description"></p>
+        </article>
+        <article class="news__article">
+            <img src="" alt="" class="article__img">
+            <h2 class="article__title"></h2>
+            <p class="article__description"></p>
+        </article>
+        <article class="news__article">
+            <img src="" alt="" class="article__img">
+            <h2 class="article__title"></h2>
+            <p class="article__description"></p>
+        </article>
+    </main>
+</body>
+</html>
